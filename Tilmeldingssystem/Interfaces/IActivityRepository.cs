9a4a8bf8619568e8de0534fd49c8e9a3bc4f0d6e
Mbah Elvis Ndum <130441973+Elvisco1989@users.noreply.github.com>
2025-07-05 09:33:55 +0200

    Add repository interfaces for Club, Member, Activity, Registration, and Payment

---
+++ Tilmeldingssystem/Interfaces/IActivityRepository.cs
@@ -0,0 +1,14 @@
+﻿using Tilmeldingssystem.Models;
+
+namespace Tilmeldingssystem.Interfaces
+{
+    public interface IActivityRepository
+    {
+        IEnumerable<Activity> GetAllActivities();
+        Activity GetActivityById(int id);
+        void AddActivity(Activity activity);
+        void UpdateActivity(Activity activity);
+        void DeleteActivity(int id);
+        bool SaveChanges();
+    }
+}
---
+++ Tilmeldingssystem/Interfaces/IRegistrationRepository.cs
@@ -0,0 +1,14 @@
+﻿using Tilmeldingssystem.Models;
+
+namespace Tilmeldingssystem.Interfaces
+{
+    public interface IRegistrationRepository
+    {
+        IEnumerable<Registration> GetAllRegistrations();
+        Registration GetRegistrationById(int id);
+        void AddRegistration(Registration registration);
+        void UpdateRegistration(Registration registration);
+        void DeleteRegistration(int id);
+        bool SaveChanges();
+    }
+}
