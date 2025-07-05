c5249391b201ffe2cc762b81e1624fbe8caec78e
Mbah Elvis Ndum <130441973+Elvisco1989@users.noreply.github.com>
2025-07-05 10:13:07 +0200

    Add Db context and Entityframework

--- Tilmeldingssystem/Models/Registration.cs
+++ Tilmeldingssystem/Models/Registration.cs
@@ -1,6 +1,14 @@
-﻿namespace Tilmeldingssystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tilmeldingssystem.Models
 {
     public class Registration
     {
+        [Key]
+        public int Id { get; set; }
+        public int MemberId { get; set; }
+        public int ActivityId { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string Status { get; set; }  // fx "betalt", "venter"
     }
 }
